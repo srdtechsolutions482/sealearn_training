1bff91ac96e5b223c0865f5ee39460cd0739e7a9
srdtechsolutions482 <srdtechsolutions@outlook.com>
2025-12-05 17:24:54 +0530

    Proxy

---
+++ functions/proxy.js
@@ -0,0 +1,13 @@
+import fetch from "node-fetch";
+
+export const handler = async (event) => {
+  const response = await fetch("http://13.201.58.203:8000/register", {
+    method: "POST",     });
+
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
